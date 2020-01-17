abbfb03a284e1912561beaca3beb8fd986b1c798
nisse26a <43852261+nisse26a@users.noreply.github.com>
2020-01-17 22:53:31 +0100

    Animation

--- script.js
+++ script.js
@@ -3,3 +3,40 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 	});
 });
+
+$(document).ready(function () {
+    $(window).scroll(function(){
+        var ScrollTop = parseInt($(window).scrollTop());
+        console.log(ScrollTop);
+
+        if (ScrollTop > 1000) {
+					document.getElementById("box4").classList.add("slideInLeft");
+					document.getElementById("box4").style.display = "block";
+        }
+    });
+});
+
+$(document).ready(function () {
+    $(window).scroll(function(){
+        var ScrollTop = parseInt($(window).scrollTop());
+        console.log(ScrollTop);
+
+        if (ScrollTop > 1200) {
+					document.getElementById("box5").classList.add("slideInRight");
+					document.getElementById("box5").style.display = "block";
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    $(window).scroll(function(){
+        var ScrollTop = parseInt($(window).scrollTop());
+        console.log(ScrollTop);
+
+        if (ScrollTop > 1400) {
+					document.getElementById("box6").classList.add("slideInLeft");
+					document.getElementById("box6").style.display = "block";
+        }
+    });
+});
