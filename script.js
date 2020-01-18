33a689e2b033c550ab4630283158f6832622ef81
nisse26a <43852261+nisse26a@users.noreply.github.com>
2020-01-18 22:04:42 +0100

    Färg/bildändringar och småfixar

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-$(document).scroll(function () {
-    var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
-    if (scrollPercent > 55) {
-        $('#box4').addClass("slideInLeft");
-				$('#box4').show();
-    }
+$(document).scroll(function() {
+  var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
+  if (scrollPercent > 55) {
+    $('#box4').addClass("slideInLeft");
+    $('#box4').show();
+  }
 });
 
-$(document).scroll(function () {
-    var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
-    if (scrollPercent > 70) {
-        $('#box5').addClass("slideInRight");
-				$('#box5').show();
-    }
+$(document).scroll(function() {
+  var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
+  if (scrollPercent > 70) {
+    $('#box5').addClass("slideInRight");
+    $('#box5').show();
+  }
 });
 
-$(document).scroll(function () {
-    var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
-    if (scrollPercent > 85) {
-        $('#box6').addClass("slideInLeft");
-				$('#box6').show();
-    }
+$(document).scroll(function() {
+  var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
+  if (scrollPercent > 85) {
+    $('#box6').addClass("slideInLeft");
+    $('#box6').show();
+  }
 });
